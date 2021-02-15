ba49986b54ad0050e659eae7aad875ae9d018e31
dtinth <dtinth@spacet.me>
2021-02-15 16:03:13 +0000

    analytics

--- gen.php
+++ gen.php
@@ -92,7 +92,15 @@ foreach ($v['sources'] as $sourceUrl => $date) {
 print_footer();
     ?>
       </footer>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.2.7/firebase-app.js"></script>
 
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.2.7/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 </body>
 <?php
 $page = ob_get_clean();
