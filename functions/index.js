95075acd990121bff999334b3d90e5407bdfa4e7
dtinth on MBP M1 <dtinth@spacet.me>
2021-03-01 01:59:48 +0700

    implement ingest endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-exports.ingest = functions.https.onRequest((request, response) => {
-  // functions.logger.info('Hello logs!', {
-  //   structuredData: true,
-  // })
-  response.send('Hello from Firebase!')
+exports.ingest = functions.https.onRequest(async (request, response) => {
+  if (request.body.key !== functions.config().ingest.key) {
+    response.status(401).send('invalid key')
+  }
+
+  try {
+    const updates = {}
+    for (const [key, value] of Object.entries(request.body.data)) {
+      updates[`${key}/updated_at`] = value.updated_at
+      updates[`${key}/retweet_count`] = value.retweet_count
+      updates[`${key}/favorite_count`] = value.favorite_count
+    }
+    await admin.database().ref('live/rooms').update(updates)
+    response.send('OK!')
+  } catch (error) {
+    console.error(error)
+    response.send('Hello from Firebase!')
+  }
 })
